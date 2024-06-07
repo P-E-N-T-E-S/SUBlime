886ea691ae5b8867d013b055bb89d1ddeffe373b
Thomazrlima <thomazlima211@gmail.com>
2024-06-07 18:17:22 -0300

    Tela legenda

--- SUBlime/src/Tela/TelaLegenda.java
+++ SUBlime/src/Tela/TelaLegenda.java
@@ -1,4 +1,68 @@
 package Tela;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class TelaLegenda {
+    private JFrame frame;
+    private JTextArea textAreaLegenda;
+
+    public TelaLegenda() {
+        initialize();
+    }
+
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 450, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+
+        JLabel lblEscreverLegenda = new JLabel("Escrever Legenda:");
+        lblEscreverLegenda.setBounds(35, 30, 200, 16);
+        frame.getContentPane().add(lblEscreverLegenda);
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(35, 60, 350, 150);
+        frame.getContentPane().add(scrollPane);
+
+        textAreaLegenda = new JTextArea();
+        scrollPane.setViewportView(textAreaLegenda);
+
+        JButton btnSalvar = new JButton("Salvar");
+        btnSalvar.setBounds(150, 230, 100, 29);
+        frame.getContentPane().add(btnSalvar);
+        btnSalvar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                salvarLegenda();
+            }
+        });
+    }
+
+    private void salvarLegenda() {
+        String legendaTexto = textAreaLegenda.getText();
+
+        if (legendaTexto != null && !legendaTexto.isEmpty()) {
+
+            JOptionPane.showMessageDialog(frame, "Legenda salva com sucesso!");
+            limparCampos();
+        } else {
+            JOptionPane.showMessageDialog(frame, "O texto da legenda está vazio!");
+        }
+    }
+
+
+    private void limparCampos() {
+        textAreaLegenda.setText("");
+    }
+
+    public void mostrarTela() {
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        TelaLegenda telaLegenda = new TelaLegenda();
+        telaLegenda.mostrarTela();
+    }
 }
